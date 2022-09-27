a0d55b195019b93127d34160b1d93f554f2abacb
dongyeop3813 <ehdduq1503@gmail.com>
2022-09-27 11:33:35 +0900

    Add implementation of quick encode table.
    Add implementation of quick encode table.
    - codeBits
    - convert
    - mergeCodeTables
    - quickEncode

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -278,7 +278,16 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  // def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  import java.lang.IllegalArgumentException
+
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = {
+    def equal_to_target_char(elem:(Char, List[Bit])): Boolean = (elem._1 == char)
+
+    table.find(equal_to_target_char) match {
+      case Some((key, bits)) => bits
+      case None => throw new IllegalArgumentException
+    }
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -288,14 +297,37 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  // def convert(tree: CodeTree): CodeTable = ???
+
+  def convert(tree: CodeTree): CodeTable = {
+    tree match {
+      case Fork(left, right, chars, weight) => {
+        val left_code_table = convert(left)
+        val right_code_table = convert(right)
+        mergeCodeTables(left_code_table, right_code_table)
+      }
+      case Leaf(char, weight) => {
+        List((char, List[Bit]()))
+      }
+    }
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  // def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(left: CodeTable, right: CodeTable): CodeTable = {
+
+    def append_some(some:Bit)(elem: (Char, List[Bit])) = (elem._1, some :: elem._2)
+
+    val append_zero = append_some(0)(_)
+    val append_one = append_some(1)(_)
+
+    val updated_left = left.map(append_zero)
+    val updated_right = right.map(append_one)
+
+    updated_left ::: updated_right
+  }
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -303,5 +335,14 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  // def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    def code_table = {
+      println("Hello")
+      convert(tree)
+    }
+
+    def encode = codeBits(code_table)(_)
+
+    text.map(encode).flatten
+  }
 }
